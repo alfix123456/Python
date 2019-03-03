b5c11605854c706568f112283326b8fed12aed81
wigugu <abc@yahoo.com>
2019-03-04 01:56:45 +0330

    20190304

---
+++ app.py
@@ -0,0 +1,9 @@
+name=input("Insert Your name: ")
+color=input("What color do you like {} ? ".format(name))
+print(name," Likes ",color)
+
+"""
+person = input('Enter your name: ')
+greeting = 'Hello, {}!'.format(person)
+print(greeting)
+"""
